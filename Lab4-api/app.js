8bc1869e19cd7991dcb090b67aa074fb74e8d195
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-03-10 11:27:30 +0100

    get latitude and longitude from location

--- Lab4-api/app.js
+++ Lab4-api/app.js
@@ -1,7 +1,28 @@
 class App{
     constructor(){
-        console.log("test constructor");
+        
+
+        this.getLocation();
+
+
     }
+
+
+    getLocation(){
+        navigator.geolocation.getCurrentPosition(this.succes);
+
+        
+    }
+
+    succes(pos){
+        
+        let lat = pos.coords.latitude;
+        let lng = pos.coords.longitude;
+
+        console.log(lat + " " + lng);
+        
+    }
+
 }
 
 

c0cf668eb8b67af5f18e0b4ee47801495cdef82a
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-03-23 16:02:20 +0100

    added pug for homepage

--- lab5-api/app.js
+++ lab5-api/app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const routerMessages = require("./routers/messages");
+const pug = require("pug");
+
+app.set("view engine", "pug");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', { title: 'Lab5 - api', message: 'Hi welcome to lab5 - api!' })
 })
 
 app.use("/api/v1/messages", routerMessages);

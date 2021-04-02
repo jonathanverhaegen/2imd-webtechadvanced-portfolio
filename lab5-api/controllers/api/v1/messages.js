722ed90040e6a4c53b5c236290fb6304b7aa8a46
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-04-02 12:53:57 +0200

    error codes when searching for a message with id that not exist

--- lab5-api/controllers/api/v1/messages.js
+++ lab5-api/controllers/api/v1/messages.js
@@ -58,6 +58,11 @@ function getOne(req,res){
                     "message": doc
                 }
             })
+        }else{
+            res.json({
+                "status": "error",
+                "message": `couldn't find a message with id = ${id}`
+            })
         }
     })
 
@@ -119,6 +124,11 @@ function update(req,res){
                     "message": doc
                 }
             })
+        }else{
+            res.json({
+                "status": "error",
+                "message": `couldn't find a message to update with id = ${id} `
+            })
         }
     })
 }
@@ -131,6 +141,11 @@ function deleteOne(req,res){
             res.json({
                 "status": "succes",
                 "message": "the message was removed"
+            })
+        }else{
+            res.json({
+                "status": "error",
+                "message": `couldn't find a message to delete with id = ${id} `
             })
         }
     })
